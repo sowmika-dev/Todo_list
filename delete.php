f031a7c833928247aa2f144c03b3511d38b43045
Sowmika V <sowmika9751@gmail.com>
2025-12-11 15:09:20 +0000

    task deletion added

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+include 'database.php';
+
+if(!isset($_GET['id']))    {
+    header("Location: index.php");
+    exit;
+}
+else {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM tasks where id = $id";
+    $result = mysqli_query($conn,$sql);
+
+    if($result){
+        header("Location:index.php?deleted=1");
+    }
+    else {
+        header("Location:index.php?deleted=0");
+    }
+}
--- index.php
+++ index.php
@@ -20,6 +20,18 @@ include 'database.php' ?>
         </nav>
     </header>
 
+
+    <!-- delete msg -->
+     <?php if(isset($_GET['deleted']) && $_GET['deleted'] == 1):
+        ?>
+        <div class="alert alert-success test center">Task deleted successfully!</div>
+     <? endif; 
+     
+        if(isset($_GET['deleted']) && $_GET['deleted'] == 0) :?>
+        <div class="alert alert-danger test center">Failed to delete task.</div>
+     <? endif; ?>
+       
+
     <main class="container mt-4 mb-5">
         <h1 class="fw-bold text-center mb-4">All Tasks</h1>
         <div class="card rounded shadow-sm">
@@ -32,7 +44,6 @@ include 'database.php' ?>
                                 $result = mysqli_query($conn,$sql);
 
                                 if (mysqli_num_rows($result)>0) {
-                                    while ($row = mysqli_fetch_assoc($result)) : 
                             ?>
                         <thead class="table-dark">
                             <tr>
@@ -46,6 +57,7 @@ include 'database.php' ?>
                         </thead>
                         <tbody>
                             <tr>
+                        <?while ($row = mysqli_fetch_assoc($result)) : ?>
                                 <td><? echo $row['id'] ?></td>
                                 <td><? echo $row['task_name'] ?></td>
                                 <td><? echo $row['description'] ?></td>
@@ -53,11 +65,29 @@ include 'database.php' ?>
                                 <td><span class="badge bg-warning text-dark"><? echo $row['status'] ?></span></td>
                                 <td>
                                     <a href="edit.php?ID=1" class="btn btn-primary btn-sm me-2">Edit</a>
-                                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal">Delete</button>
+                                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal<?= $row['id'] ?>">Delete</button>
                                 </td>
-                                <? endwhile; 
-                                ?>
-                            </tr>
+                            <div class="modal fade" id="deleteModal<?= $row['id']?>" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                            <div class="modal-header bg-dark text-white">
+                                <h5 class="modal-title" id="deleteModalLabel">Confirm delete</h5>
+                                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                             Are you sure you want to delete this task?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel
+                                </button>
+
+                                <a  class="btn btn-danger" id="confirmDelete" href="delete.php?id=<? echo $row['id']?>">Delete</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </tr>
+        <? endwhile;  ?>
                         </tbody>
                     </table>
                 </div>
@@ -72,26 +102,6 @@ include 'database.php' ?>
                                 ?>
         </div>
     </main>
-
-        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header bg-dark text-white">
-                        <h5 class="modal-title" id="deleteModalLabel">Confirm delete</h5>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Are you sure you want to delete this task?
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel
-                        </button>
-                        <button type="button" class="btn btn-danger" id="confirmDelete">Delete
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
     
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
    
