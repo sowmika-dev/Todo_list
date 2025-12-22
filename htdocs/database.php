1f036630531971b9fdb4616bb3e350d4941208fe
Sowmika V <sowmika9751@gmail.com>
2025-12-22 15:03:49 +0000

    database configuration changed

--- htdocs/database.php
+++ htdocs/database.php
@@ -1,11 +1,25 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "sowmika_123sql";
-$db = "Todo_app";
+$configFile = __DIR__ . '/../workspace/config.json';
 
-$conn = mysqli_connect($host,$user,$pass,$db);
+if(!is_readable($configFile)) {
+    die('Database config file not found');
+}
+
+$config = json_decode(file_get_contents($configFile),true);
+
+if(!$config){
+    die('Invalid database config file');
+}
+
+$conn = mysqli_connect(
+    $config['host'],
+    $config['user'],
+    $config['pass'],
+    $config['db']
+);
 
 if(!$conn) {
     die("database connection failed".mysqli_connect_error());
 }
+
+return $conn;
